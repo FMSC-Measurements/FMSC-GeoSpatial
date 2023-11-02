5403184df81a55d9fe5b3f565ef6f66a5a6a5de9
bmillerfs <>
2023-11-01 18:34:24 -0600

    Update BaseCommand.java

--- src/main/java/com/usda/fmsc/geospatial/ins/vectornav/commands/BaseCommand.java
+++ src/main/java/com/usda/fmsc/geospatial/ins/vectornav/commands/BaseCommand.java
@@ -1,7 +1,7 @@
 package com.usda.fmsc.geospatial.ins.vectornav.commands;
 
-import com.usda.fmsc.geospatial.ins.vectornav.nmea.codes.MessageID;
-import com.usda.fmsc.geospatial.ins.vectornav.nmea.codes.RegisterID;
+import com.usda.fmsc.geospatial.ins.vectornav.codes.MessageID;
+import com.usda.fmsc.geospatial.ins.vectornav.codes.RegisterID;
 
 public abstract class BaseCommand {
     public static final String DELIMITER = ",";
